bee457ed6a63a31c2378559b1b7f6cc9735db219
Mads M. Hansen <madsmh@gmail.com>
2020-02-17 14:40:11 +0100

    Corrected copyright notice.

--- objects/box.h
+++ objects/box.h
@@ -2,21 +2,20 @@
  *
  * This file is part of Tracy.
 
- MMRenderer is free software: you can redistribute it and/or modify
+ Tracy is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- MMRenderer is distributed in the hope that it will be useful,
+ Tracy is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with MMRender.  If not, see <http://www.gnu.org/licenses/>.
+ along with Tracy.  If not, see <http://www.gnu.org/licenses/>.
  *
  * */
-
 
 #ifndef TRACY_BOX_H
 #define TRACY_BOX_H
--- utilities/kahan.h
+++ utilities/kahan.h
@@ -1,30 +1,27 @@
-//
-// Created by mads on 14/02/2020.
-//
+/* Copyright (c) 2020 By the Authors
+ *
+ * This file is part of Tracy.
+
+ Tracy is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Tracy is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Tracy.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
 
 #ifndef TRACY_KAHAN_H
 #define TRACY_KAHAN_H
 
 #include <vector>
 
-/* Copyright (c) 2020 By the Authors
- *
- * This file is part of Tracy.
-
- MMRenderer is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- MMRenderer is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with MMRender.  If not, see <http://www.gnu.org/licenses/>.
- *
- * */
 
 
 // implements the Kahan summation algorithm as described at
